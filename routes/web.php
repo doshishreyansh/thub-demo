f2080106be80ce12bc09f6a3ac4b1bddbaebf461
wiztechlabs <admin@wiztechlabs.com>
2022-10-03 11:15:52 +0530

    added default route

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::controller(ProductController::class)->group(function () {
     Route::post('product', 'store');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+// });
+
+Route::get('/', [ProductController::class, 'productList']);
